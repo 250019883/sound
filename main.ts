9c375bd2aa115c0830d55097aabe33d5f881b3d1
250019883 <105178592+250019883@users.noreply.github.com>
2022-11-20 12:37:57 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
-input.onSound(DetectedSound.Loud, function () {
-    basic.showIcon(IconNames.Sad)
-    music.ringTone(988)
-    basic.pause(1000)
-    music.stopAllSounds()
-})
 input.onSound(DetectedSound.Quiet, function () {
     basic.showIcon(IconNames.Happy)
 })
-input.setSoundThreshold(SoundThreshold.Loud, 40)
 input.setSoundThreshold(SoundThreshold.Quiet, 30)
+basic.forever(function () {
+    if (input.soundLevel() > 40) {
+        basic.showIcon(IconNames.Sad)
+        music.ringTone(988)
+    } else {
+        music.stopAllSounds()
+    }
+})
 basic.forever(function () {
     if (input.soundLevel() > 30) {
         if (input.soundLevel() < 40) {
